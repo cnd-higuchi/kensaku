fbb61e0543ecbf444a52d1d2486402c2000cf2cd
Higuchi Naoya <n-higuchi@cn-door.com>
2015-07-24 10:27:42 +0900

    ide-helper setup

--- config/app.php
+++ config/app.php
@@ -143,6 +143,11 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        /*
+         * IDE Helper...
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
